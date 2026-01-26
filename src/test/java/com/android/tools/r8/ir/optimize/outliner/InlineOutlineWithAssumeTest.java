acb084da7e94ff0cb439aa25149754bc5872613c
Christoffer Adamsen <christofferqa@google.com>
2026-01-26 13:22:06 +0100

    Reproduce AssertionError from inlining outline with assume instructions
    This reproduces the following error seen from landing https://r8.googlesource.com/r8/+/4edaac7153ccae07d3dcddd35a1ef014e7d7599a:
    
    java.lang.AssertionError
    	at com.android.tools.r8.graph.Code.newInlineePosition(Code.java:244)
    	at com.android.tools.r8.ir.code.CanonicalPositions.<init>(CanonicalPositions.java:49)
    	at com.android.tools.r8.lightir.LirCode.getPositionTableAsInlining(LirCode.java:732)
    	at com.android.tools.r8.lightir.Lir2IRConverter$Parser.<init>(Lir2IRConverter.java:240)
    	at com.android.tools.r8.lightir.Lir2IRConverter.translate(Lir2IRConverter.java:148)
    	at com.android.tools.r8.lightir.LirCode.internalBuildIR(LirCode.java:604)
    	at com.android.tools.r8.lightir.LirCode.buildInliningIR(LirCode.java:587)
    	at com.android.tools.r8.graph.ProgramMethod.buildInliningIR(ProgramMethod.java:63)
    
    Change-Id: Ide7c45cd3bbc5234d79c8b77a0abc648232e0ce3

---
+++ src/test/java/com/android/tools/r8/ir/optimize/outliner/InlineOutlineWithAssumeTest.java
@@ -0,0 +1,112 @@
+// Copyright (c) 2026, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.ir.optimize.outliner;
+
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.graph.DexEncodedMethod;
+import com.android.tools.r8.graph.DexItemFactory;
+import com.android.tools.r8.graph.DexProgramClass;
+import com.android.tools.r8.graph.ThrowNullCode;
+import java.util.Objects;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class InlineOutlineWithAssumeTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters)
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .addKeepRules("-keep class " + Main.class.getTypeName() + " { void sink(...); }")
+        .addOptionsModification(
+            options -> {
+              options.outline.threshold = 2;
+              options.outline.minSize = 2;
+              options.getTestingOptions().enqueuerInspector =
+                  (appView, mode) -> {
+                    // Replace the body of otherOutlineCallSite() by `throw null` after the second
+                    // round of tree shaking so that the outline gets single call inlined in the
+                    // backed.
+                    if (mode.isFinalTreeShaking()) {
+                      DexItemFactory factory = appView.dexItemFactory();
+                      DexProgramClass mainClass =
+                          appView
+                              .definitionFor(factory.createType(descriptor(Main.class)))
+                              .asProgramClass();
+                      DexEncodedMethod otherOutlineCallSiteMethod =
+                          mainClass.lookupMethod(
+                              m -> m.getName().isEqualTo("otherOutlineCallSite"));
+                      otherOutlineCallSiteMethod.setCode(
+                          ThrowNullCode.get(), otherOutlineCallSiteMethod.getParameterInfo());
+                    }
+                  };
+            })
+        .enableInliningAnnotations()
+        .compile()
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("Hello, world!");
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      outlineCallSite();
+      try {
+        otherOutlineCallSite();
+      } catch (NullPointerException e) {
+        System.out.println("!");
+      }
+    }
+
+    @NeverInline
+    static void outlineCallSite() {
+      String hello = System.currentTimeMillis() > 0 ? "Hello" : null;
+      // Perform a null check so that we won't materialize an AssumeNotNull instruction after the
+      // call to hello().
+      if (hello != null) {
+        Greeter.hello(hello);
+        // <-- No AssumeNotNull instruction.
+        Greeter.world();
+      }
+    }
+
+    @NeverInline
+    static void otherOutlineCallSite() {
+      String hello = System.currentTimeMillis() > 0 ? "Hello" : null;
+      Greeter.hello(hello);
+      // <-- AssumeNotNull instruction, since the normal return of hello() ensures hello != null.
+      Greeter.world();
+    }
+  }
+
+  static class Greeter {
+
+    @NeverInline
+    static void hello(String hello) {
+      Objects.requireNonNull(hello);
+      System.out.print(hello);
+    }
+
+    @NeverInline
+    static void world() {
+      System.out.print(", world");
+    }
+  }
+}
