cad114f6d25604c27a2855b6b42707e7635ce490
Christoffer Adamsen <christofferqa@google.com>
2026-02-10 15:49:35 +0100

    Add --dump-blast-radius-to-directory to compiledump.py
    Change-Id: I333372f54d629eff52c799652eec7a73ec2bbb94

--- tools/compiledump.py
+++ tools/compiledump.py
@@ -42,6 +42,9 @@ def make_parser():
                         '--compiler',
                         help='Compiler to use',
                         default=None)
+    parser.add_argument('--dump-blast-radius-to-directory',
+                        '--dump_blast_radius_to_directory',
+                        help='Dump blast radius to the given directory')
     parser.add_argument('--dump-trace-to-directory',
                         '--dump_trace_to_directory',
                         help='Dump perfetto trace to the given directory')
@@ -662,6 +665,9 @@ def run1(out, args, otherargs, jdkhome=None, worker_id=None):
         if args.enable_test_assertions:
             cmd.append('-Dcom.android.tools.r8.enableTestAssertions=1')
         feature_jars = dump.feature_jars()
+        if args.dump_blast_radius_to_directory is not None:
+            cmd.append('-Dcom.android.tools.r8.dumpblastradiustodirectory=' +
+                       args.dump_blast_radius_to_directory)
         if args.dump_trace_to_directory is not None:
             cmd.append('-Dcom.android.tools.r8.dumptracetodirectory=' +
                        args.dump_trace_to_directory)
