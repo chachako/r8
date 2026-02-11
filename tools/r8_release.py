5df15afd3b6e317a99c8379385d2132b25c3c19a
Søren Gjesse <sgjesse@google.com>
2026-02-11 10:18:05 +0100

    Prepare tools/r8_release.py for branch 9.2
    Change-Id: Id8d25461acc63e20e9cad02d20d5fd296a89cc4f

--- tools/r8_release.py
+++ tools/r8_release.py
@@ -18,7 +18,7 @@ import zipfile
 import git_utils
 import utils
 
-R8_DEV_BRANCH = '9.1'
+R8_DEV_BRANCH = '9.2'
 R8_VERSION_FILE = os.path.join('src', 'main', 'java', 'com', 'android', 'tools',
                                'r8', 'Version.java')
 THIS_FILE_RELATIVE = os.path.join('tools', 'r8_release.py')
