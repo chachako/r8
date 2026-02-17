3342e410bfacae7f7dbcb7eab6feea1c44ce1491
Christoffer Adamsen <christofferqa@google.com>
2026-02-17 10:47:15 +0100

    Add an empty tools/analyze_gmaven.py script for running on bots
    Bug: b/483568398
    Change-Id: Id252e81e0d2384d471e95383547c5dae2c3b254a

---
+++ tools/analyze_gmaven.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright (c) 2026, the R8 project authors. Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+def main():
+    print('Running analyze_gmaven.py')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
