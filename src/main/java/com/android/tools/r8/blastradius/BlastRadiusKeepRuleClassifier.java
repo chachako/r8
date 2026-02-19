644d26470966d48d2e8e3880422a50de9939a720
Christoffer Adamsen <christofferqa@google.com>
2026-02-19 10:38:35 +0100

    Refine package wide classification of keep rules
    This updates the classification so that for -keepclassmembers rules, the
    member rules must be sufficiently broad:
    
    * Keep all members
    * Keep all fields
    * Keep all methods
    * Keep all default constructors
    
    This also updates the classification so that -if rules are classified as
    package wide rules if the subsequent rule is a package wide rule.
    
    Bug: b/485430452
    Change-Id: Ibf76abbbfd9b927b25642908a7b30d93dbf7cdae

--- src/main/java/com/android/tools/r8/blastradius/BlastRadiusKeepRuleClassifier.java
+++ src/main/java/com/android/tools/r8/blastradius/BlastRadiusKeepRuleClassifier.java
@@ -5,15 +5,24 @@ package com.android.tools.r8.blastradius;
 
 import com.android.tools.r8.shaking.ProguardClassNameList;
 import com.android.tools.r8.shaking.ProguardKeepRuleBase;
+import com.android.tools.r8.shaking.ProguardKeepRuleType;
+import com.android.tools.r8.shaking.ProguardMemberRule;
+import com.android.tools.r8.shaking.ProguardMemberType;
+import com.android.tools.r8.shaking.ProguardTypeMatcher;
 import com.android.tools.r8.shaking.ProguardTypeMatcher.MatchTypePattern;
 import com.android.tools.r8.utils.TraversalContinuation;
+import java.util.List;
 
 public class BlastRadiusKeepRuleClassifier {
 
   public static boolean isPackageWideKeepRule(ProguardKeepRuleBase rule) {
-    if (rule.isProguardIfRule() || rule.hasClassAnnotations() || rule.hasInheritanceClassName()) {
+    if (rule.isProguardIfRule()) {
+      return isPackageWideKeepRule(rule.asProguardIfRule().getSubsequentRule());
+    }
+    if (rule.hasClassAnnotations() || rule.hasInheritanceClassName()) {
       return false;
     }
+    // Must have exactly one class name that ends in .* or .**.
     ProguardClassNameList classNames = rule.getClassNames();
     if (classNames == null || classNames.size() != 1) {
       return false;
@@ -28,6 +37,41 @@ public class BlastRadiusKeepRuleClassifier {
               }
               return TraversalContinuation.doContinue();
             });
-    return traversalContinuation.isBreak();
+    if (traversalContinuation.isContinue()) {
+      return false;
+    }
+    // If it's a -keep rule, then this is already bad.
+    if (rule.getType() == ProguardKeepRuleType.KEEP) {
+      return true;
+    }
+    // If it's a -keepclassmembers rule, then the member rules must be sufficiently broad for the
+    // rule to be classified as package wide.
+    if (rule.getType() == ProguardKeepRuleType.KEEP_CLASS_MEMBERS) {
+      for (ProguardMemberRule memberRule : rule.getMemberRules()) {
+        ProguardMemberType memberType = memberRule.getRuleType();
+        // Package wide if type is all.
+        if (memberType == ProguardMemberType.ALL
+            || memberType == ProguardMemberType.ALL_FIELDS
+            || memberType == ProguardMemberType.ALL_METHODS) {
+          return true;
+        }
+        // Package wide if it matches all class initializers.
+        if (memberType == ProguardMemberType.CLINIT) {
+          return true;
+        }
+        // Package wide if it matches at least all default instance initializers.
+        if (memberType == ProguardMemberType.INIT) {
+          List<ProguardTypeMatcher> arguments = memberRule.getArguments();
+          if (arguments == null || arguments.isEmpty() || arguments.get(0).isTripleDotPattern()) {
+            return true;
+          }
+        }
+      }
+    }
+    // If it's a -keepclasseswithmembers rule, then report it.
+    if (rule.getType() == ProguardKeepRuleType.KEEP_CLASSES_WITH_MEMBERS) {
+      return true;
+    }
+    return false;
   }
 }
